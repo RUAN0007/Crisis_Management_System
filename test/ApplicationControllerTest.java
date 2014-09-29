cfba369ca122355f0c832eac88fccb59fc0d5e33
Ruan Pingcheng <ruanpingcheng@gmail.com>
2014-09-29 21:29:09 +0800

    Replace assert with assertThat

--- test/ApplicationControllerTest.java
+++ test/ApplicationControllerTest.java
@@ -31,76 +31,75 @@ import static org.fest.assertions.Assertions.*;
 */
 public class ApplicationControllerTest {
 
-//   @Test
-//   public void testLogOut(){
-//	   
-//	   running(fakeApplication(), new Runnable() {
-//		    public void run() {
-//		    	  FakeRequest fakeLogOutRequest = new FakeRequest("GET","/logout");
-//		   	   
-//		   	   Result result = Helpers.routeAndCall(fakeLogOutRequest.withSession("id", "asfasfasdf"));
-//		   	   String content = contentAsString(result);
-//		   	   JsonNode json = Json.parse(content);
-//		   	   assert(Integer.parseInt(json.get("error").toString()) == 0)
-//		   	   	:json.get("message").toString()	;
-//		   	   }
-//		 });
-//   }
+   @Test
+   public void testLogOut(){
+	   
+	   running(fakeApplication(), new Runnable() {
+		    public void run() {
+		    	  FakeRequest fakeLogOutRequest = new FakeRequest("GET","/logout");
+		   	   
+		   	   Result result = Helpers.routeAndCall(fakeLogOutRequest.withSession("id", "asfasfasdf"));
+		   	   String content = contentAsString(result);
+		   	   JsonNode json = Json.parse(content);
+		   	   assertThat(Integer.parseInt(json.get("error").toString())).isEqualTo(0);
+		   	   }
+		 });
+   }
 //   
-//   @Test
-//   public void testGetEventsBytypeID(){
-//	   
-//	   running(fakeApplication(), new Runnable() {
-//		    public void run() {
-//		    	  FakeRequest fakeLogOutRequest = new FakeRequest("POST","/events");
-//		   	   Map<String,String> paras = new HashMap<>();
-//		   	   paras.put("typeID","1");
-//		   	   paras.put("timePeriodInHour", "1");
-//		   	   Result result = Helpers.routeAndCall(fakeLogOutRequest.withFormUrlEncodedBody(paras));
-//		   	   String content = contentAsString(result);
-//		   	   JsonNode json = Json.parse(content);
-//		   	   assert(Integer.parseInt(json.get("error").toString()) == 0)
-//		   	   	:json.get("message").toString()	;
-//		   	   assert(json.get("events").size() == 2)
-//		   	   	:"The event list size is " + json.get("events").size();
-//		   	   }
-//		 });
-//   }
+   @Test
+   public void testGetEventsBytypeID(){
+	   
+	   running(fakeApplication(), new Runnable() {
+		    public void run() {
+		    	  FakeRequest fakeLogOutRequest = new FakeRequest("POST","/events");
+		   	   Map<String,String> paras = new HashMap<>();
+		   	   paras.put("typeID","1");
+		   	   paras.put("timePeriodInHour", "1");
+		   	   Result result = Helpers.routeAndCall(fakeLogOutRequest.withFormUrlEncodedBody(paras));
+		   	   String content = contentAsString(result);
+		   	   JsonNode json = Json.parse(content);
+		   	   
+		   	   assertThat(Integer.parseInt(json.get("error").toString())).isEqualTo(0);
+
+		   	   assertThat(json.get("events").size()).isEqualTo(2);
+		   	   }
+		 });
+   }
 //   
-//   @Test
-//   public void testGetEventTypes(){
-//	   
-//	   running(fakeApplication(), new Runnable() {
-//		    public void run() {
-//		    	  FakeRequest fakeLogOutRequest = new FakeRequest("GET","/getEventTypes");
+   @Test
+   public void testGetEventTypes(){
+	   
+	   running(fakeApplication(), new Runnable() {
+		    public void run() {
+		    	  FakeRequest fakeLogOutRequest = new FakeRequest("GET","/getEventTypes");
+		   	 
+		   	   Result result = Helpers.routeAndCall(fakeLogOutRequest);
+		   	   String content = contentAsString(result);
+		   	   JsonNode json = Json.parse(content);
+		   	   assertThat(Integer.parseInt(json.get("error").toString())).isEqualTo(0);
+
+		   	   assertThat(json.get("eventTypes").size()).isEqualTo(5);
+		    }	   
+		 });
+	   
+   }
+	
+  @Test
+  public void testGetAgencies(){
+	   
+	   running(fakeApplication(), new Runnable() {
+		    public void run() {
+		    	  FakeRequest fakeLogOutRequest = new FakeRequest("GET","/getAgencies");
 //		   	 
-//		   	   Result result = Helpers.routeAndCall(fakeLogOutRequest);
-//		   	   String content = contentAsString(result);
-//		   	   JsonNode json = Json.parse(content);
-//		   	   assert(Integer.parseInt(json.get("error").toString()) == 0)
-//		   	   	:json.get("message").toString()	;
-//		   	   assert(json.get("eventTypes").size() == 5)
-//		   	   	:"The eventType list size is " + json.get("events").size();
-//		   	   }
-//		 });
-//   }
-//	
-//  @Test
-//  public void testGetAgencies(){
-//	   
-//	   running(fakeApplication(), new Runnable() {
-//		    public void run() {
-//		    	  FakeRequest fakeLogOutRequest = new FakeRequest("GET","/getAgencies");
-////		   	 
-//		   	   Result result = Helpers.routeAndCall(fakeLogOutRequest);
-//		   	   String content = contentAsString(result);
-//		   	   JsonNode json = Json.parse(content);
-//		   	   assert(Integer.parseInt(json.get("error").toString()) == 0)
-//		   	   	:json.get("message").toString()	;
-//		   	   assert(json.get("Agencies").size() == 6)
-//		   	   	:"The Agencies list size is " + json.get("events").size();
-//		   	   }
-//		 });
-//  }
+		   	   Result result = Helpers.routeAndCall(fakeLogOutRequest);
+		   	   String content = contentAsString(result);
+		   	   JsonNode json = Json.parse(content);
+		   	   assertThat(Integer.parseInt(json.get("error").toString())).isEqualTo(0);
+
+		   	   assertThat(json.get("Agencies").size()).isEqualTo(5);
+
+		   	   }
+		 });
+  }
 
 }
