032b5c26a999459abe9d3c2493593be01063de41
Ruan Pingcheng <ruanpingcheng@gmail.com>
2014-10-10 19:22:58 +0800

    Separate the tasks of sending routine email

--- app/Global.java
+++ app/Global.java
@@ -1,6 +1,8 @@
 import com.avaje.ebean.Ebean;
 
 import controllers.EventCenter;
+import models.Agency;
+import models.Event;
 
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -12,7 +14,13 @@ import play.libs.Yaml;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -34,117 +42,15 @@ public class Global extends GlobalSettings {
 	@Override
 	public void onStart(Application app) {
 		//System.out.println("I hate YAML!!");
-		if(!EventCenter.setDefaultEventCenter(getEventCenter())){
-			System.err.println("Create eventCenter failed...");
-		}
 
 		//TODO
 		//Finish testing
 		//Comment it out 
-//		int periodInMs = this.periodForSummaryReportInMin * 60 * 1000;
-//		summaryTimer.scheduleAtFixedRate(new TimerTask() {
-//
-//			@Override
-//			public void run() {
-//				System.out.println("Sending Report...");
-//				EventCenter.getDefaultEventCenter()
-//					.sendSummaryReport(periodForSummaryReportInMin);
-//			}
-//		}, 0,periodInMs);
+		int periodInMs = this.periodForSummaryReportInMin * 60 * 1000;
+		summaryTimer.scheduleAtFixedRate(new RoutineEmailTask(periodForSummaryReportInMin), 0,periodInMs);
 	}
 
-	private static EventCenter getEventCenter(){
-		try{
-			File file = new File("conf" + File.separator + "paras.xml");
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(file);
-			doc.getDocumentElement().normalize();
-
-			EmailSender emailSender = getEmailSender(doc);
-			SMSSender smsSender = getSMSSender(doc);
-			FacebookSender fbSender = getFBSender(doc);
-
-			TwitterSender twitterSender = getTwitterSender(doc);
-			EventFormatter eventFormatter = new EventFormatter();
-			PDFGenerator pdfGenerator = getReportGenerator(doc);
-			
-			return new EventCenter(emailSender, smsSender, fbSender, twitterSender, eventFormatter, pdfGenerator);
-		
-		}catch(Exception e){
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	private static FacebookSender getFBSender(Document doc) throws XPathExpressionException {
-		XPathFactory xFactory = XPathFactory.newInstance();
-		XPath xpath = xFactory.newXPath();		
-		XPathExpression expr = xpath.compile("/paras/facebook/accessToken");
-		String accessToken = (String) expr.evaluate(doc,XPathConstants.STRING);
 	
-		expr = xpath.compile("/paras/facebook/accessTokenSecret");
-		String accessTokenSecret = (String) expr.evaluate(doc,XPathConstants.STRING);
-		//TODO Testing
-	//	return new FacebookSender(accessToken, accessTokenSecret);
-		return null;
-	
-	}
-	
-	private static TwitterSender getTwitterSender(Document doc) throws XPathExpressionException {
-		XPathFactory xFactory = XPathFactory.newInstance();
-		XPath xpath = xFactory.newXPath();		
-		XPathExpression expr = xpath.compile("/paras/twitter/consumerKey");
-		String consumerKey = (String) expr.evaluate(doc,XPathConstants.STRING);
-	
-		expr = xpath.compile("/paras/twitter/consumerSecret");
-		String consumerSecret = (String) expr.evaluate(doc,XPathConstants.STRING);
-		
-		expr = xpath.compile("/paras/twitter/accessToken");
-		String accessToken = (String) expr.evaluate(doc,XPathConstants.STRING);
-		
-		expr = xpath.compile("/paras/twitter/accessTokenSecret");
-		String accessTokenSecret = (String) expr.evaluate(doc,XPathConstants.STRING);
-		
-		return new TwitterSender(consumerKey,consumerSecret,accessToken, accessTokenSecret);
-	}
-	
-	private static EmailSender getEmailSender(Document doc) throws XPathExpressionException {
-		XPathFactory xFactory = XPathFactory.newInstance();
-		XPath xpath = xFactory.newXPath();		
-		XPathExpression expr = xpath.compile("/paras/email/account");
-		String account = (String) expr.evaluate(doc,XPathConstants.STRING);
-	
-		expr = xpath.compile("/paras/email/password");
-		String password = (String) expr.evaluate(doc,XPathConstants.STRING);
-		
-		return new EmailSender(account, password);
-	
-	}
-	
-	private static PDFGenerator getReportGenerator(Document doc) throws XPathExpressionException{
-		XPathFactory xFactory = XPathFactory.newInstance();
-		XPath xpath = xFactory.newXPath();		
-		XPathExpression expr = xpath.compile("/paras/email/emergency");
-		String emergencyReportDir = (String) expr.evaluate(doc,XPathConstants.STRING);
-	
-		expr = xpath.compile("/paras/email/summary");
-		String summaryReportDir = (String) expr.evaluate(doc,XPathConstants.STRING);
-		
-		return new PDFGenerator(emergencyReportDir,summaryReportDir);
-	}
-
-	private static SMSSender getSMSSender(Document doc) throws XPathExpressionException{
-		XPathFactory xFactory = XPathFactory.newInstance();
-		XPath xpath = xFactory.newXPath();		
-		XPathExpression expr = xpath.compile("/paras/sms/sid");
-		String sid = (String) expr.evaluate(doc,XPathConstants.STRING);
-	
-		expr = xpath.compile("/paras/sms/token");
-		String token = (String) expr.evaluate(doc,XPathConstants.STRING);
-		
-		return new SMSSender(sid,token);
-	}
 	
 	@Override
 	public void onStop(Application app) {
@@ -154,12 +60,5 @@ public class Global extends GlobalSettings {
 
 	}
 
-	static class InitialData {
-
-		public static void insert(Application app) {
-			//	            TODO
-			//Start a timer to send Email for every 30 min
-		}
-
-	}
+	
 }
---
+++ app/RoutineEmailTask.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimerTask;
+
+import models.Agency;
+import models.Event;
+import broadcaster.EmailSender;
+import formatter.PDFGenerator;
+
+
+public class RoutineEmailTask extends TimerTask {
+	private EmailSender emailSender = ResourceGenerator
+			.getDefaultResourceGenerator()
+			.getNewEmailSender();
+	
+	private PDFGenerator pdfGenerator = ResourceGenerator
+			.getDefaultResourceGenerator()
+			.getNewReportGenerator();
+	
+	private int periodForSummaryReportInMin;
+	public RoutineEmailTask(int periodForSummaryReportInMin) {
+		this.periodForSummaryReportInMin = periodForSummaryReportInMin;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Sending Report...");
+
+		List<Event> events = getEventsWithinMin(periodForSummaryReportInMin);
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy_hh:mm",Locale.US);
+		Date date = new Date();
+		String time = dateFormat.format(date);
+		
+		String subject = "Summary Report";
+		String text = "Dear Prime Minister, \nThis is a summary event at " + time 
+				+ ". Refer to the attachment for details."; 
+		String reportName = "SummaryReportAt" + time + ".pdf";
+		
+		File report = pdfGenerator.generateReport(events, reportName);
+		String reportFilePath = pdfGenerator.getSummaryReportDirectory() + File.separator + report.getName();
+		
+		List<String> destinations = new ArrayList<String>();
+		destinations.add(getPMEmail());
+		 if(emailSender.sendMail(destinations, subject, text, reportFilePath)){
+			 System.out.println("Sending succeeded...");
+		 }else{
+			 System.out.println("Sending failed...");
+		 }
+		
+		
+	}
+	
+	private List<Event> getEventsWithinMin(int periodInMin){
+		Timestamp lowerTimeBound = new Timestamp(System.currentTimeMillis() - periodInMin * 60 * 1000);
+		List<Event> events = Event.find.where()
+									   .gt("callingTime", lowerTimeBound)
+									   .orderBy("callingTime desc")
+
+									   .findList();
+		return events;
+	}
+	
+	
+	private String getPMEmail(){
+		Agency primeMinster = Agency.find.byId((long)0);
+		return primeMinster.getEmail();
+	}
+
+}
